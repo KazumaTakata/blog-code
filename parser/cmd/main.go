b5bda05d6e08bc97d96dd90e192415c402891878
Kazuma Takata <neilbuzztakata@gmail.com>
2020-01-27 21:10:29 +0900

    lr1 start state

--- parser/cmd/main.go
+++ parser/cmd/main.go
@@ -16,7 +16,14 @@ type State_element struct {
 	Offset       int
 }
 
+type State_element_with_follow struct {
+	state_element State_element
+	follow        string
+}
+
 type State map[State_element]bool
+
+type State_with_follow map[State_element_with_follow]bool
 
 type State_with_next struct {
 	next  map[string]int
@@ -39,6 +46,60 @@ func Expand_non_terminal(state_element State_element, bnf_list []util.Bnf) State
 						new_state := Expand_non_terminal(new_state_element, bnf_list)
 						for new_ele, _ := range new_state {
 							added_state[new_ele] = true
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	return added_state
+}
+
+func get_follow(state_element State_element, bnf_list []util.Bnf) (bool, string) {
+
+	if state_element.Offset+1 < len(bnf_list[state_element.Product_id].Right[state_element.Alternate_id]) {
+		return true, bnf_list[state_element.Product_id].Right[state_element.Alternate_id][state_element.Offset+1]
+	}
+
+	return false, ""
+}
+
+func Expand_non_terminal_with_follow(state_element State_element, bnf_list []util.Bnf, follow string, current_state State_with_follow) State_with_follow {
+
+	non_terminals := util.Get_nonterminal(bnf_list)
+	added_state := make(State_with_follow)
+
+	if state_element.Offset < len(bnf_list[state_element.Product_id].Right[state_element.Alternate_id]) {
+		right_ele := bnf_list[state_element.Product_id].Right[state_element.Alternate_id][state_element.Offset]
+		if _, ok := non_terminals[right_ele]; ok {
+			for prod_id, bnf := range bnf_list {
+				if bnf.Left == right_ele {
+					for alte_id, _ := range bnf.Right {
+						new_state_element := State_element{Product_id: prod_id, Alternate_id: alte_id, Offset: 0}
+						new_state_element_with_follow := State_element_with_follow{state_element: new_state_element, follow: follow}
+
+						if _, ok := current_state[new_state_element_with_follow]; ok {
+							return State_with_follow{}
+						}
+
+						added_state[new_state_element_with_follow] = true
+						current_state[new_state_element_with_follow] = true
+
+						exist, new_follow := get_follow(new_state_element, bnf_list)
+
+						follow_arg := ""
+						if exist {
+							follow_arg = new_follow
+						} else {
+							follow_arg = follow
+						}
+						new_state_with_follow := Expand_non_terminal_with_follow(new_state_element, bnf_list, follow_arg, current_state)
+
+						for new_ele, _ := range new_state_with_follow {
+							added_state[new_ele] = true
+							current_state[new_ele] = true
 						}
 					}
 				}
@@ -153,19 +214,13 @@ func (q *not_explored_queue) empty() bool {
 
 	return false
 }
-
-func lr0_automata(filepath string) []State_with_next {
-
-	bnf, err := ioutil.ReadFile(filepath)
-	util.Check(err)
-
-	bnf_parsed := util.Parse_bnf_file(string(bnf))
+func gen_start_state(bnf_list []util.Bnf) State {
 	start_state := make(State)
 	start_state[State_element{Product_id: 0, Alternate_id: 0, Offset: 0}] = true
 
 	new_start_state := make(State)
 	for state_ele, _ := range start_state {
-		new_elements := Expand_non_terminal(state_ele, bnf_parsed)
+		new_elements := Expand_non_terminal(state_ele, bnf_list)
 		for new_ele, _ := range new_elements {
 			new_start_state[new_ele] = true
 		}
@@ -173,6 +228,38 @@ func lr0_automata(filepath string) []State_with_next {
 	for new_elem, _ := range new_start_state {
 		start_state[new_elem] = true
 	}
+
+	return start_state
+
+}
+func gen_start_state_with_follow(bnf_list []util.Bnf) State_with_follow {
+	start_state := make(State_with_follow)
+	start_element := State_element{Product_id: 0, Alternate_id: 0, Offset: 0}
+	start_element_follow := State_element_with_follow{state_element: start_element, follow: "$"}
+	start_state[start_element_follow] = true
+
+	new_start_state := make(State_with_follow)
+	for state_ele, _ := range start_state {
+		new_elements := Expand_non_terminal_with_follow(state_ele.state_element, bnf_list, start_element_follow.follow, State_with_follow{})
+		for new_ele, _ := range new_elements {
+			new_start_state[new_ele] = true
+		}
+	}
+	for new_elem, _ := range new_start_state {
+		start_state[new_elem] = true
+	}
+
+	return start_state
+
+}
+func lr0_automata(filepath string) []State_with_next {
+
+	bnf, err := ioutil.ReadFile(filepath)
+	util.Check(err)
+
+	bnf_parsed := util.Parse_bnf_file(string(bnf))
+
+	start_state := gen_start_state(bnf_parsed)
 
 	automaton_states := []State_with_next{}
 	_, root_index := add_to_automaton_states(&automaton_states, start_state)
@@ -192,16 +279,44 @@ func lr0_automata(filepath string) []State_with_next {
 	return automaton_states
 }
 
+func lr1_automata(filepath string) State_with_follow {
+
+	bnf, err := ioutil.ReadFile(filepath)
+	util.Check(err)
+
+	bnf_parsed := util.Parse_bnf_file(string(bnf))
+
+	start_state := gen_start_state_with_follow(bnf_parsed)
+
+	//automaton_states := []State_with_next{}
+	//_, root_index := add_to_automaton_states(&automaton_states, start_state)
+	//not_explored_queue := not_explored_queue{queue: []int{root_index}}
+
+	//for !not_explored_queue.empty() {
+	//root_index := not_explored_queue.dequeu()
+	//root_state := automaton_states[root_index]
+	//new_states := create_new_states(bnf_parsed, root_state.state)
+	//not_explored := add_all_to_automaton_states(&automaton_states, root_index, new_states)
+
+	//for _, index := range not_explored {
+	//not_explored_queue.enqueu(index)
+	//}
+	/*}*/
+
+	return start_state
+}
 func main() {
 
 	_, filename, _, _ := runtime.Caller(0)
-	bnf_path := filepath.Join(filepath.Dir(filepath.Dir(filename)), "sample2.bnf")
+	bnf_path := filepath.Join(filepath.Dir(filepath.Dir(filename)), "sample3.bnf")
 
-	automaton_states := lr0_automata(bnf_path)
+	automaton_states := lr1_automata(bnf_path)
 
-	for _, state := range automaton_states {
-		fmt.Printf("%v\n", state)
-	}
+	fmt.Printf("%v", automaton_states)
+
+	//for _, state := range automaton_states {
+	//fmt.Printf("%v\n", state)
+	//}
 
 	//removed := ll.Remove_direct_left_recursion(bnf_parsed)
 	//nonterminal_set := util.Get_nonterminal(removed)
